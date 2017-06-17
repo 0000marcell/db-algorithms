2259892eb133082509ad836c07d7c0bbcf4ac491
Marcell Monteiro Cruz <0000marcell@gmail.com>
2017-06-17 15:18:53 -0300

    run_length_encoding problem fixed

--- run_length_encoding.rb
+++ run_length_encoding.rb
@@ -2,20 +2,9 @@ require_relative './lib/utils'
 require_relative './lib/run_length_encoding'
 
 rows = generate_data()
-rows = bubble_sort(rows, "country")
+quick_sort(rows, "country")
 tp rows, ["name", "address", "country"]
-columns = column_layout(rows)
-
-db = {}
-columns.each do |k, v|
-  db[k] = {"dic" => [], "av" => []}
-end
-
-# create dic and av
-db["country"]["dic"] = create_dic(columns["country"])
-db["country"]["av"] = create_av(columns["country"],
-                               db["country"]["dic"])
-
+db = create_db(rows)
 run_length_encoding(db["country"])
 puts "final result prefix encoded!"
 puts "av *****"
